e7c10c28dba5f64f52c560a061145055f7fecc7a
Alex B <ajbarbati@gmail.com>
2019-05-22 23:03:41 -0400

    started to declare variables

---
+++ assets/javascript/game.js
@@ -0,0 +1,9 @@
+var words = [
+    "cheese",
+    "fruit",
+    "carrots",
+    "meat",
+    "bread"
+]                                                   
+
+var word =word[Math.floor(Math.random() * words.length)]

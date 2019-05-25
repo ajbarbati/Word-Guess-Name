b150e5c0d9ebd8bcffec2879c044372b6f4f033d
Alex B <ajbarbati@gmail.com>
2019-05-25 15:12:12 -0400

    had some more code writtebn out on my desktop

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,4 @@
+//Creating the list of words
 var words = [
     "cheese",
     "fruit",
@@ -5,5 +6,23 @@ var words = [
     "meat",
     "bread"
 ]                                                   
+
+//setting a variable to make the array random
+var word = words[Math.floor(Math.random() * words.length)]
+
+// Get Js to recognize a Key Press
+    // if keypress equals a letter of the array(word) print to screen 
+        // else lower number of guesses by 1 and add letter to already guessed
+ document.onkeypress = function(a) {
+       
+ }
+// Track and Display Wins
+
+// Display Blank Word Spaces and get Js to display correct letter when neccesary
+
+// Show the Number of Guesses remaining
+
+// display letters already guessed, and stop repeat letters counting as a guess
+
 
 var word =word[Math.floor(Math.random() * words.length)]
